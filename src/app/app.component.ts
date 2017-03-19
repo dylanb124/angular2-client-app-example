0943250acda9fc25d898140801c429f3cbb3d510
Dylan Barnes <dylanb124@gmail.com>
2017-03-19 15:32:42 -0500

    4. Inject service into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SampleService } from 'angular2-library-example';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  // Inject sample serivce
+  constructor(private sampleService: SampleService) {
+
+  }
 }
